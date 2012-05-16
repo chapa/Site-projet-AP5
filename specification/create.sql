dd2ddf09cac986a3e26312f8e7accd7f80ebdc5b
vincent <vincent.dimper@gmail.com>
2012-05-16 17:43:13 +0200

    ajout specifications

---
+++ specification/create.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Series
+
+(
+	id 				numeric(5) ,
+	slug			varchar(100),
+	title			varchar(100),
+	originalTitle	varchar(100),
+	synopsis		text,
+	nbSeasons		numeric(2),
+	nbEpisodes		numeric(3),
+	creator			varchar(30),
+	actor			text,
+	yearStart		numeric(4),
+	
+
+)
