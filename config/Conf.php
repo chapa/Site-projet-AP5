bde4c45b4da595eb75bd232f0f3eb0e110ee655f
Mickaël <mickael.bourgier@gmail.com>
2012-05-21 16:51:19 +0200

    Ajout du core Model (connexion à la base + méthodes query et find) et de Conf::

--- config/Conf.php
+++ config/Conf.php
@@ -3,4 +3,12 @@
 	class Conf
 	{
 		static $debug = true;
+		static $databases = array(
+			'default' => array(
+				'host'     => 'postgres-info',
+				'database' => 'based109',
+				'login'    => 'userd109',
+				'password' => 'p09'
+			)
+		);
 	}
--- core/Model.php
+++ core/Model.php
@@ -2,5 +2,99 @@
 
 	class Model
 	{
-		
+		static $connexions = array();	// Liste des connexions aux bases de données
+		public $conf = 'default';		// Nom de la base de donnée à laquelle se connecter (à configurer dans /config/conf.php)
+		public $table = false;			// Nom de la table dans laquelle aller chercher les informations
+		public $db;						// Objet PDO de la connexion à la base de données
+		public $primaryKey = 'id';
+		public $foreignKeys = false;
+		public $errors = array();
+		public $errorEmptyField = 'Vous devez remplir ce champ';
+
+		public function __construct()
+		{
+			// Vérification que l'on ne se soit pas déjà connecté à la base de données
+			$conf = Conf::$databases[$this->conf];
+			if(isset(Model::$connexions[$this->conf]))
+			{
+				$this->db = Model::$connexions[$this->conf];
+			}
+			else
+			{
+				// Connexion à la base de données et placement de l'objet PDO de la connexion dans Model::$connexions['nom_base']
+				try
+				{
+					$pdo = new PDO(
+						'pgsql:host=' . $conf['host'] . ';dbname=' . $conf['database'] . ';',
+						$conf['login'],
+						$conf['password']
+					);
+					if(Conf::$debug >= 1)
+						$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+					Model::$connexions[$this->conf] = $pdo;
+					$this->db = $pdo;
+				}
+				catch(PDOException $e)
+				{
+					if(Conf::$debug >= 1)
+						die($e->getMessage());
+					else
+						die('Connexion impossible à la base de données');
+				}
+			}
+
+			// Si $this->table ne contient pas le nom de la table demandée, on la met
+			if($this->table === false)
+			{
+				$this->table = lcfirst(get_class($this));
+				if($this->table[strlen($this->table) - 1] == 'y')
+				{
+					$this->table[strlen($this->table) - 1] = 'i';
+					$this->table .= 'es';
+				}
+				elseif($this->table[strlen($this->table) - 1] != 's')
+					$this->table .= 's';
+			}
+		}
+
+		public function query($sql)
+		{
+			$pre = $this->db->prepare($sql);
+			$pre->execute();
+
+			$r = $pre->fetchAll(PDO::FETCH_OBJ);
+			return $r;
+		}
+
+		public function find($req = array())
+		{
+			$sql = 'SELECT ';
+
+			if(!empty($req['fields']))
+				$sql .= $req['fields'] . ' ';
+			else
+				$sql .= '* ';
+
+			$sql .= 'FROM ';
+
+			if(!empty($req['tables']))
+				$sql .= $req['tables'] . ' ';
+			else
+				$sql .= $this->table . ' ';
+
+			$sql .= 'WHERE ';
+
+			if(!empty($req['conditions']))
+				$sql .= $req['conditions'];
+			else
+				$sql .= 'true';
+
+			if(!empty($req['order']))
+				$sql .= ' ORDER BY ' . $req['order'];
+
+			if(!empty($req['limit']))
+				$sql .= ' LIMIT ' . $req['limit'];
+
+			return $this->query($sql);
+		}
 	}
