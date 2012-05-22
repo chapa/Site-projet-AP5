8ac9d174839abf35c551cccc8fc79fc78abe4c22
vinc38 <vincent.dimper@gmail.com>
2012-05-22 21:34:31 +0200

    specification create.sql

--- specification/create.sql
+++ specification/create.sql
@@ -1,7 +1,9 @@
+
+
 CREATE TABLE Series
 
 (
-	id 				numeric(5) ,
+	id 				numeric(3) ,
 	slug			varchar(100),
 	title			varchar(100),
 	originalTitle	varchar(100),
@@ -11,6 +13,63 @@ CREATE TABLE Series
 	creator			varchar(30),
 	actor			text,
 	yearStart		numeric(4),
-	
+	yearEnd			numeric(4),
+	mark			numeric(1),
+	formatTime		numeric(1),
+	type			varchar(30),
+	nationality 	varchar(30),
+	numwatched		numeric(6),
+	primary key (id)
 
-)
+);
+
+CREATE TABLE Seasons
+(
+	id				numeric(5),
+	num 			numeric(2),
+	nbEpisodess		numeric(2),
+	yearStart		numeric(2),
+	yearEnd 		numeric(2),
+	mark			numeric(1),
+	numwatched		numeric(6),
+	idS				numeric(3) 
+	primary key(id),
+	foreign key(idS) references Series(id)
+);
+
+CREATE TABLE Episodes(
+id			numeric(5),
+num 		numeric(2),
+title 		varchar(100),
+originalTitle text,
+synopsis 	text,
+mark 		numeric(1),
+watched		boolean,
+numwatched  numeric(6),
+idS			numeric(5),
+primary key(id),
+foreign key(idS) references Seasons(id)
+);
+
+
+CREATE TABLE Users
+(
+id 			numeric(5),
+username	varchar(20),
+password	varchar(256),
+mail		varchar(50),
+created		date,
+lastlogin 	date,
+primary key (id)
+
+
+);
+
+CREATE TABLE regarde(
+idU			numeric(5),
+idE			numeric(5),
+primary key (idE,idU),
+foreign key (idU) references Users(id),
+foreign key (idE) references Users(Episodes)
+);
+
