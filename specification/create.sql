cd6c219865ecc6c1430eda64a9b4957445e86ddd
Mickaël <mickael.bourgier@gmail.com>
2012-05-23 10:20:49 +0200

    Correction du sql (create+drop)

--- specification/create.sql
+++ specification/create.sql
@@ -1,70 +1,75 @@
 CREATE TABLE Series
 (
-	id 				numeric(3) ,
-	slug			varchar(100),
-	title			varchar(100),
-	originalTitle	varchar(100),
-	synopsis		text,
-	nbSeasons		numeric(2),
-	nbEpisodes		numeric(3),
-	creator			varchar(30),
-	actor			text,
-	yearStart		numeric(4),
-	yearEnd			numeric(4),
-	mark			numeric(1),
-	formatTime		numeric(1),
-	type			varchar(30),
-	nationality 	varchar(30),
-	numwatched		numeric(6),
+	id 						integer,
+	slug					varchar(255),
+	title					varchar(255),
+	originalTitle			varchar(255),
+	synopsis				text,
+	nbSeasons				numeric(3),
+	nbEpisodes				numeric(3),
+	creators				varchar(255),
+	actors					varchar(255),
+	yearstart				numeric(4),
+	yearend					numeric(4),
+	mark					numeric(2,1),
+	formatTime				numeric(3),
+	type					varchar(255),
+	nationality 			varchar(255),
+	nbwatched				integer,
+
 	primary key (id)
 );
 
 CREATE TABLE Seasons
 (
-	id				numeric(5),
-	num 			numeric(2),
-	nbEpisodess		numeric(2),
-	yearStart		numeric(2),
-	yearEnd 		numeric(2),
-	mark			numeric(1),
-	numwatched		numeric(6),
-	idS				numeric(3),
+	id						integer,
+	num 					numeric(3),
+	nbEpisodes				numeric(3),
+	yearstart				numeric(4),
+	yearend 				numeric(4),
+	mark					numeric(2,1),
+	nbwatched				integer,
+	serie_id				integer,
+
 	primary key (id),
-	foreign key (idS) references Series(id)
+	foreign key (serie_id) references Series(id)
 );
 
 CREATE TABLE Episodes
 (
-	id				numeric(5),
-	num 			numeric(2),
-	title 			varchar(100),
-	originalTitle	text,
-	synopsis 		text,
-	mark 			numeric(1),
-	watched			boolean,
-	numwatched  	numeric(6),
-	idS				numeric(5),
+	id						integer,
+	num 					numeric(3),
+	title 					varchar(255),
+	originalTitle			varchar(255),
+	synopsis 				text,
+	mark 					numeric(2,1),
+	nbwatched  				integer,
+	season_id				integer,
+
 	primary key (id),
-	foreign key (idS) references Seasons(id)
+	foreign key (season_id) references Seasons(id)
 ); 
 
 
 CREATE TABLE Users
 (
-	id 				numeric(5),
-	username		varchar(20),
-	password		varchar(256),
-	mail			varchar(50),
-	created			date,
-	lastlogin 		date,
+	id 						integer,
+	username				varchar(255),
+	password				varchar(256),
+	mail					varchar(255),
+	created					timestamp,
+	lastlogin 				timestamp,
+
 	primary key (id)
 );
 
 CREATE TABLE Watch
 (
-	idU				numeric(5),
-	idE				numeric(5),
-	primary key (idE,idU),
-	foreign key (idU) references Users(id),
-	foreign key (idE) references Episodes(id)
+	user_id					integer,
+	episode_id				integer,
+	watched					boolean,
+
+	primary key (episode_id,user_id),
+	foreign key (user_id) references Users(id),
+	foreign key (episode_id) references Episodes(id)
 );
