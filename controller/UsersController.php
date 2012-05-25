dca6ca44fb273da9e8f2a21c48c39a518c6d4404
chapa <mickael.bourgier@gmail.com>
2012-05-25 16:21:32 +0200

    Modification controller users (login/logout opérationnel)

--- controller/UsersController.php
+++ controller/UsersController.php
@@ -4,34 +4,58 @@
 	{
 		public function login()
 		{
-			if(!empty($this->request->data))
+			if(empty($_SESSION['user']))
 			{
-				if($this->User->validate($this->request->data, 'login'))
+				if(!empty($this->request->data))
 				{
-					$d = $this->User->findFirst(array(
-						'fields' => 'id, status',
-						'conditions' => array(
-							'username' => $this->request->data['username'],
-							'password' => sha1($this->request->data['password'])
-						)
-					));
-
-					if(!empty($d))
+					if($this->User->validate($this->request->data, 'login'))
 					{
-						$_SESSION['user']['id'] = $d->id;
-						$_SESSION['user']['status'] = $d->status;
-						//Faire la redirection
-						$this->Session->setFlash('Redirection ... Vous avez bien été connecté ... enfin bientot :-°');
+						$d = $this->User->findFirst(array(
+							'fields' => 'id, status',
+							'conditions' => array(
+								'username' => $this->request->data['username'],
+								'password' => sha1($this->request->data['password'])
+							)
+						));
+
+						if(!empty($d))
+						{
+							$_SESSION['user']['id'] = $d->id;
+							$_SESSION['user']['status'] = $d->status;
+
+							$this->Session->setFlash('Vous êtes mainenant connecté');
+							$this->redirect('/');
+						}
+						else
+						{
+							$this->Session->setFlash('Le nom d\'utilisateur ou le mot de passe est incorrect, veuillez réessayer', 'error');
+						}
 					}
 					else
 					{
-						$this->Session->setFlash('Le nom d\'utilisateur ou le mot de passe est incorrect, veuillez réessayer', 'error');
+						$this->Session->setFlash('Erreur lors de la saisie des champs', 'error');
 					}
 				}
-				else
-				{
-					$this->Session->setFlash('Erreur lors de la saisie des champs', 'error');
-				}
 			}
+			else
+			{
+				$this->Session->setFlash('Vous êtes déjà connecté', 'warning');
+				$this->redirect('/');
+			}
+		}
+
+		public function logout()
+		{
+			if(!empty($_SESSION['user']))
+			{
+				session_destroy();
+				session_start();
+				$this->Session->setFlash('Vous êtes bien déconnecté');
+			}
+			else
+			{
+				$this->Session->setFlash('Vous n\'êtes pas encore connecté', 'warning');
+			}
+			$this->redirect('/');
 		}
 	}
