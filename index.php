b57ec43567f7a2147d489815a735806fdac3eb7c
chapa <mickael.bourgier@gmail.com>
2012-05-29 18:23:09 +0200

    Déplacement de la bannière de temps de chargement de la page dans les layouts
    Du coup le temps est un peu moins juste (pas de beaucoup) mais les
    pages sont valides

--- index.php
+++ index.php
@@ -12,6 +12,3 @@
 	require_once(CORE . DS . 'includes.php');
 
 	new Dispatcher(str_replace(BASE_URL . DS, '', $_SERVER['REQUEST_URI']));
-
-	if(Conf::$debug)
-		echo '<div class="alert alert-block alert-success fade in" style="position:fixed;bottom:0;left:0;right:0;margin:10px;"><a href="#" class="close" data-dismiss="alert">&times;</a>Page généré en ' . round(microtime(true) - $timeStart, 5) . ' secondes</div>';
--- view/layout/error.php
+++ view/layout/error.php
@@ -50,6 +50,12 @@
 			echo $this->Html->js('bootstrap-carousel');
 			echo $this->Html->js('bootstrap-typeahead');
 			echo $this->Html->js('application');
+
+			if(Conf::$debug)
+			{
+				global $timeStart;
+				echo '<div class="alert alert-block alert-success fade in" style="position:fixed;bottom:0;left:0;right:0;margin:10px;"><a href="#" class="close" data-dismiss="alert">&times;</a>Page généré en ' . round(microtime(true) - $timeStart, 5) . ' secondes</div>';
+			}
 		?>
 
 	</body>
