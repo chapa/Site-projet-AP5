3af0cff6932465feb8bab40f0221c9de22c4dd3a
vinc38 <vincent.dimper@gmail.com>
2012-06-01 14:07:24 +0200

    mod

--- controller/EpisodesController.php
+++ controller/EpisodesController.php
@@ -10,30 +10,44 @@ class EpisodesController extends AppController
 
 			$user_id = $_SESSION['user']['id'];
 
-			$d = $this->Serie->findFirst(array(
+			$season_id= $this->Episode->findFirst(array(
+				'fields'=> 'season_id',
+				'tables' => 'Episodes',
+				'conditions' => array( 'id' =>(int) $id)
+			));
+			
+			$serie_id= $this->Episode->findFirst(array(
+				'fields'=> 'serie_id',
+				'tables' => 'seasons',
+				'conditions' => array( 'id' => $season_id['season_id'])
+			));
+			$d = $this->Episode->findFirst(array(
 				'tables' => 'Watch',
-				'conditions' => array('user_id' => $user_id, 'serie_id' => $id)
+				'conditions' => array('user_id' => $user_id, 'serie_id' => $serie_id['serie_id'])
 			));
 			if(empty($d))
 			{
 				$this->Session->setFlash('Vous ne suivez pas cette saison', 'error');
 				$this->redirect(array('controller' => 'series', 'action' => 'liste'), 404);
 			}
-
-			$d = $this->Serie->find(array(
-				'fields' => 'id',
-				'tables' => 'Episodes',
-				'conditions' => array('serie_id' => $id)
-			));
-			$episodes = array();
-			foreach($d as $v) {
-				$e = $this->Serie->find(array(
-					'fields' => 'id',
-					'tables' => 'episodes',
-					'conditions' => array('season_id' => $v['id'])
-				));
-				$episodes = array_merge($episodes, $e);
+			if ($action==1)// episode non vu ->  vu
+			{
+				$this->Episode->table='EpisodesWatched';
+				$this->Episode->save(array(
+					'user_id'=>$user_id,
+					'episode_id'=>$id));
+				$this->Session->setFlash('La série a bien été marquée comme vue');
 			}
+			else if($action==0 )
+			{
+				$this->Episode->table='EpisodesWatched';
+				$this->Episode->delete(array(
+					'user_id'=>$user_id,
+					'episode_id'=>$id));
+				$this->Session->setFlash('La série a bien été marquée comme non vue');
+			}
+
+			
 
 			
 			$this->redirect(array('action' => 'serie', $id));
