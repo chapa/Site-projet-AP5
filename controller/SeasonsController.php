1112c803dc0cbec3c2389c81910d89219c977f3a
vinc38 <vincent.dimper@gmail.com>
2012-06-01 09:40:16 +0200

    ajout SeasonsController

---
+++ controller/SeasonsController.php
@@ -0,0 +1,149 @@
+<?php
+
+class SeasonsController extends AppController
+{
+	public function season($season_id=0,$user_id=0)
+	{
+		
+		if(!empty($_SESSION))
+		{
+			$info_episodes=$this->Season->find(array(
+				'fields' => '*',
+				'tables' => 'episodes',
+				'conditions' => array('season_id' => $season_id),
+				'order'=> 'num'
+				));
+			if(!empty($info_episodes))//la serie existe
+			{
+
+
+				
+				
+
+
+				if(empty($user_id))
+				{
+					$user_id=$_SESSION['user']['id'];
+				}
+				$d = $this->Season->findFirst(array(
+					'fields' => 'username',
+					'tables' => 'users',
+					'conditions' => array('id' => (int) $user_id)
+					));
+				
+				$this->set('username', $d['username']);
+				
+				
+				
+
+				
+				//cherche la progression de la serie (0 ou 100)
+				$serie_id=$this->Season->findFirst(array(
+					'fields' => 'Serie_id,num',
+					'tables' => 'Seasons',
+					'conditions' => array('id' => $season_id)
+					));
+
+				$serie= $this->Season->findFirst(array(
+
+						'tables' =>	'series',
+						'conditions' => array('id' => $serie_id['serie_id'])
+					));
+				
+				$serie['actors'] = explode(', ', $serie['actors']);
+				$progression=$this->Season->findFirst(array(
+					'fields' => 'progression',
+					'tables' => 'seriesWatched',
+					'conditions' => array('user_id' => $user_id,
+										'serie_id'=> $serie_id['serie_id'])
+					));
+
+
+				
+				$serie['numseason']=$serie_id['num'];
+				$d = $this->Season->findFirst(array(
+					'fields' => 'progression',
+					'tables' => 'SeriesWatched',
+					'conditions' => array('user_id' => $user_id, 'serie_id' => $serie_id['serie_id'])
+				));
+				$serie['progression'] = !empty($d) ? current($d) : 0;
+
+				if(empty($progression['progression']))
+				{
+					$progression['progression']==0;
+				}
+				if($progression['progression']==100|| $progression['progression']==0)
+				{	
+					
+					$this->set(array('progression' => $progression['progression']));
+					foreach ($info_episodes as $key => $value) {
+						
+						$info_episodes[$key]['vue']=$progression['progression']/100;
+					}
+					
+				}
+
+				else//la progression de la serie ne nous permet pas de conaitre la progression des episodes
+				{
+					unset($progression);
+					//cherche la progression de la season (0 ou 100)
+					$progression=$this->Season->findFirst(array(
+						'fields' => 'progression',
+						'tables' => 'SeasonsWatched',
+						'conditions' => array('user_id' => $user_id,
+							'season_id'=> $season_id)
+						));
+					
+					if(empty($progression['progression']))
+					{
+						$progression['progression']=0;
+					}
+
+					$this->set(array('progression' => $progression['progression']));
+					if($progression['progression']==100|| $progression['progression']==0)
+					{
+						foreach ($info_episodes as $key => $value) {
+							
+							$info_episodes[$key]['vue']=$progression['progression']/100;
+							
+						}
+						
+					}
+					else//la progression de la saison ne nous permet pas de savoir la progression des episodes
+					{
+						foreach ($info_episodes as $key => $value) {
+							$d=$this->Season->findFirst(array(
+								'fields' => 'count(*) vue',
+								'tables' => 'episodesWatched',
+								'conditions' => array('user_id' => $user_id,
+									'episode_id'=> $value['id'])
+								));
+							$info_episodes[$key]['vue']=$d['vue'];
+							
+						}
+					}
+				}
+
+			}
+			
+			else
+			{
+				$this->Session->setFlash('La saison n\'existe pas', 'error');
+				$this->redirect(array('controller' => 'series', 'action' => 'search'), 403);
+			}
+			$this->set(array('episodes' => $info_episodes,
+				'user_id' => $user_id,
+				'serie' => $serie,
+				'serie_id' => $serie_id['serie_id']
+				));
+			//debug($info_episodes);
+		}
+		
+		else
+		{
+			$this->Session->setFlash('Vous devez être connecté pour pouvoir accéder à cette partie du site', 'error');
+			$this->redirect(array('controller' => 'users', 'action' => 'login'), 403);
+		}
+	}
+}
+
---
+++ view/seasons/season.php
@@ -0,0 +1,264 @@
+<?php
+	$image = 'series' . DS . 'banners' . DS . $serie_id . '.jpg';
+	if(!is_file(WEBROOT . DS . 'img' . DS . $image))
+		$image = 'series' . DS . 'banners' . DS . '0.png';
+?>
+<div class="row-fluid">
+	<div class="span3">&nbsp;</div>
+	<div class="span6 offset3">
+		<?php echo $this->Html->image($image, array('class' => 'thumbnail')); ?>
+	</div>
+</div>
+
+<div class="page-header">
+	<h1>
+		<?php
+			
+			echo ($user_id != $_SESSION['user']['id']) ? '[' . $this->Html->link($username, array('action' => 'liste', $user_id), array('rel' => 'tooltip', 'data-original-title' => 'Afficher les saisons de ' . $username)) . '] ' : '';
+			echo $serie['title'];
+		?>
+		<small><?php echo  'saison '.$serie['numseason'] ; ?></small>
+		
+	</h1>
+</div>
+
+<div class="row-fluid">
+	<div class="span2">
+		<h3>Informations</h3>
+		<h6><i class="icon-play-circle"></i> Avancement</h6>
+			<p>
+				<div class="progress progress-info progress-striped active">
+					<div class="bar" style="width: <?php echo $progression; ?>%;"></div>
+					<div class="text"><?php echo $progression; ?> %</div>
+				</div>
+			</p>
+		<h6><i class="icon-asterisk"></i> Titre original</h6>
+			<p><?php echo $serie['originaltitle']; ?></p>
+		<h6><i class="icon-heart"></i> Note</h6>
+			<p><div class="note jDisabled" id="<?php echo $serie['mark']; ?>"></div></p>
+		<h6><i class="icon-calendar"></i> Année de création</h6>
+			<p><?php echo $serie['yearstart']; ?></p>
+		<h6><i class="icon-calendar"></i> Année d'arrêt</h6>
+		<?php if(!empty($serie['yearend'])) : ?>
+			<p><?php echo $serie['yearend']; ?></p>
+		<?php else : ?>
+			<p>Encore en production</p>
+		<?php endif; ?>
+		<h6><i class="icon-time"></i> Format des épisodes</h6>
+			<p><?php echo $serie['formattime']; ?> minutes</p>
+		<?php if(!empty($serie['creators'])) : ?>
+			<h6><i class="icon-user"></i> Réalisateur</h6>
+				<p><?php echo $serie['creators']; ?></p>
+		<?php endif; ?>
+		<h6><i class="icon-adjust"></i> Genre</h6>
+			<p><?php echo $serie['type'] ?></p>
+		<h6><i class="icon-map-marker"></i> Nationalité</h6>
+			<p><?php echo $serie['nationality']; ?></p>
+	</div>
+	<div class="span10">
+		<div class="tabbable">
+			<ul class="nav nav-tabs tabs">
+				<li>
+					<a href="#synopsis" data-toggle="tab"><i class="icon-home"></i> Synopsis</a>
+				</li>
+				<li class="active">
+					<a href="#episodes" data-toggle="tab"><i class="icon-film"></i> Episodes</a>
+				</li>
+				<li>
+					<a href="#acteurs" data-toggle="tab"><i class="icon-star"></i> Acteurs</a>
+				</li>
+				<?php if($user_id == $_SESSION['user']['id'] OR $_SESSION['user']['status'] == 'Administrateur') : ?>
+					<li class="dropdown">
+						<a href="#actions" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-cog"></i> Actions <b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<?php if($serie['progression'] == 100): ?>
+								<li><a data-toggle="modal" href="#notWatched"><i class="icon-ok"></i> Marquer la saison comme non vue</a></li>
+							<?php else: ?>
+								<li><a data-toggle="modal" href="#watched"><i class="icon-ok"></i> Marquer la saison comme vue</a></li>
+							<?php endif ?>
+							<li class="divider"></li>
+							<li><a data-toggle="modal" href="#deleteModal"><i class="icon-trash"></i> Supprimer cette série</a></li>
+						</ul>
+					</li>
+				<?php endif; ?>
+			</ul>
+			
+			<div class="tab-content">
+				<div class="tab-pane" id="synopsis">
+					<?php echo $serie['synopsis']; ?>
+				</div>
+				<div class="tab-pane active" id="episodes">
+					<?php if(3 + 3 > 0) : ?>
+						<?php if(3 > 0) : ?>
+							<h3>Saisons à voir</h3>
+							<table class="table table-bordered table-striped">
+								<thead>
+									<tr>
+										<th class="minWidth"></th>
+										<th>#</th>
+										<th>Titre</th>
+										
+										<th class="minWidth">Note</th>
+									
+									</tr>
+								</thead>
+								<tbody>
+									<?php foreach($episodes as $v) : if($v['progression'] < 100) : ?>
+										<tr>
+											<td>
+												<?php
+													if($user_id == $_SESSION['user']['id'])
+													{
+														echo $this->Html->link(
+															'Afficher',
+															array('controller' => 'seasons', 'action' => 'season', $v['id']),
+															array('class' => 'btn btn-mini')
+														);
+													}
+													else
+													{
+														echo $this->Html->link(
+															'Afficher',
+															array('controller' => 'seasons', 'action' => 'season', $v['id'], $user_id),
+															array('class' => 'btn btn-mini')
+														);
+													}
+												?>
+											</td>
+											<td><?php echo $v['num']; ?></td>
+											<td><?php echo $v['title']; ?> épisodes</td>
+											
+											<td><div class="note jDisabled" id="<?php echo $v['mark']; ?>"></div></td>
+											
+										</tr>
+									<?php endif; endforeach; ?>
+								</tbody>
+							</table>
+						<?php endif; ?>
+						<?php if($seasonsWatched > 0) : ?>
+							<h3>Saisons vues</h3>
+							<table class="table table-bordered table-striped">
+								<thead>
+									<tr>
+										<th class="minWidth">Actions</th>
+										<th>#</th>
+										<th>Titre</th>
+										
+										<th class="minWidth">Note</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php foreach($seasons as $v) : if($v['progression'] == 100) : ?>
+										<tr>
+											<td>
+												<?php
+													if($user_id == $_SESSION['user']['id'])
+													{
+														echo $this->Html->link(
+															'Afficher',
+															array('controller' => 'seasons', 'action' => 'season', $v['id']),
+															array('class' => 'btn btn-mini')
+														);
+													}
+													else
+													{
+														echo $this->Html->link(
+															'Afficher',
+															array('controller' => 'seasons', 'action' => 'season', $v['id'], $user_id),
+															array('class' => 'btn btn-mini')
+														);
+													}
+												?>
+											</td>
+											<td><?php echo $v['num']; ?></td>
+											<td><?php echo $v['title']; ?> </td>
+											
+											<td><div class="note jDisabled" id="<?php echo $v['mark']; ?>"></div></td>
+											
+										</tr>
+									<?php endif; endforeach; ?>
+								</tbody>
+							</table>
+						<?php endif; ?>
+					<?php else : ?>
+						<div class="alert alert-error centre">
+							Nous sommes désolé mais aucune saison n'a été trouvée dans la base de données de AlloCiné pour cette série
+						</div>
+					<?php endif; ?>
+				</div>
+				<div class="tab-pane" id="acteurs">
+					<ul>
+						<?php foreach($serie['actors'] as $a) : ?>
+							<li><?php echo $a; ?></li>
+						<?php endforeach; ?>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php if($user_id == $_SESSION['user']['id'] OR $_SESSION['user']['status'] == 'Administrateur') : ?>
+	<div class="modal hide fade" id="deleteModal">
+		<div class="modal-header">
+			<a class="close" data-dismiss="modal">&times;</a>
+			<h3>Attention</h3>
+		</div>
+		<div class="modal-body">
+			<p>
+				Êtes-vous sûr de bien vouloir supprimer cette série ?<br>
+				Toutes vos progressions seront alors supprimées.
+			</p>
+		</div>
+		<div class="modal-footer">
+			<?php echo $this->Html->link(
+				'Supprimer',
+				array('controller' => 'series', 'action' => 'delete', $serie['id']),
+				array('class' => 'btn btn-primary')
+			); ?>
+			<a class="btn" data-dismiss="modal">Annuler</a>
+		</div>
+	</div>
+
+	<div class="modal hide fade" id="notWatched">
+		<div class="modal-header">
+			<a class="close" data-dismiss="modal">&times;</a>
+			<h3>Attention</h3>
+		</div>
+		<div class="modal-body">
+			<p>
+				Êtes-vous sûr de bien vouloir marquer cette saison comme non vue ?<br>
+				Tous les épisodes de toutes les saisons seront alors marqués comme non vus.
+			</p>
+		</div>
+		<div class="modal-footer">
+			<?php echo $this->Html->link(
+				'Marquer la saison comme non vue',
+				array('controller' => 'series', 'action' => 'watched', $serie['id'], false),
+				array('class' => 'btn btn-primary')
+			); ?>
+			<a class="btn" data-dismiss="modal">Annuler</a>
+		</div>
+	</div>
+
+	<div class="modal hide fade" id="watched">
+		<div class="modal-header">
+			<a class="close" data-dismiss="modal">&times;</a>
+			<h3>Attention</h3>
+		</div>
+		<div class="modal-body">
+			<p>
+				Êtes-vous sûr de bien vouloir marquer cette saison comme vue ?<br>
+				Tous les épisodes de toutes les saisons seront alors marqués comme vus.
+			</p>
+		</div>
+		<div class="modal-footer">
+			<?php echo $this->Html->link(
+				'Marquer la saison comme vue',
+				array('controller' => 'series', 'action' => 'watched', $serie['id'], true),
+				array('class' => 'btn btn-primary')
+			); ?>
+			<a class="btn" data-dismiss="modal">Annuler</a>
+		</div>
+	</div>
+<?php endif; ?>
